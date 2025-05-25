fe38a17ad010d372b3f4d94f9cc9daf74f034424
bankkk1996 <bankkk1996@users.noreply.github.com>
2025-05-25 16:21:36 +0000

    deploy: b95872ebb40c6e94cf9e5c07cf240708c6b2ac98

--- app/Gallery/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-    return (
-        <div className="container m-auto px-4 pt-20">
-            Gallery
-        </div>
-    );
-}
--- app/Infinity/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-    return (
-        <div className="container m-auto p-4  pt-20">
-            Infinity
-        </div>
-    );
-}
