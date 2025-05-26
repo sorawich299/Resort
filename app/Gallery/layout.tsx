4293da9406793542d47631ff677fba20ec61b2f3
sorawich <sorawich.khe@smarttollcollection.com>
2025-05-26 14:36:31 +0700

    Update Code

--- app/Gallery/layout.tsx
+++ app/Gallery/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { ReactNode } from 'react';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Solunar. | Gallery",
