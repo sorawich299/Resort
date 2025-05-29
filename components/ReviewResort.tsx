227bce652984b58b9938fbd2e28557f0a59522b4
sorawich <sorawich.khe@smarttollcollection.com>
2025-05-29 14:11:24 +0700

    Update

--- components/ReviewResort.tsx
+++ components/ReviewResort.tsx
@@ -11,7 +11,7 @@ interface ReviewResortProps{
 export default function ReviewResort({image, title, subTitle}:ReviewResortProps) {
     return (
         <AnimatedFadeInUp>
-        <section className="relative h-[900px] w-full flex items-center justify-center text-white px-20">
+        <section className="relative h-[900px] w-full flex items-center justify-center text-white px-5 lg:px-20">
             {/* Background Image */}
             
             <Image
@@ -24,9 +24,9 @@ export default function ReviewResort({image, title, subTitle}:ReviewResortProps)
             <div className="absolute inset-0 bg-black opacity-50 z-10" />
 
             {/* Overlay Content */}
-            <div className="relative py-28 px-16 z-10 text-center w-full h-full flex flex-col justify-center items-center gap-6">
+            <div className="relative py-14 lg:py-28 px-8 lg:px-16 z-10 text-center w-full h-full flex flex-col justify-center items-center gap-6">
                 <h1 className="text-5xl font-medium text-white">{title}</h1>
-                <p className="max-w-[560px] text-lg font-normal text-white" style={{ fontFamily: '"IBM Plex Sans Thai Looped", sans-serif' }}>{subTitle}</p>
+                <p className="lg:max-w-[560px] text-lg font-normal text-white" style={{ fontFamily: '"IBM Plex Sans Thai Looped", sans-serif' }}>{subTitle}</p>
                 <button className="cursor-pointer px-7 py-3.5 bg-white text-black border border-gray-200 rounded-lg font-medium" onClick={()=>{console.log('A')}}>
                     Reserve
                 </button>
