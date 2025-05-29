7aa1b132a7f5e976ecf9d6b5e582fc862da0526b
sorawich <sorawich.khe@smarttollcollection.com>
2025-05-29 17:23:12 +0700

    Update

--- components/VideoPlayer.tsx
+++ components/VideoPlayer.tsx
@@ -7,7 +7,7 @@ export default function VedioPlayer({ videoId, className = "" }: YouTubeEmbedPro
   return (
     <div className="py-14 lg:py-28"><div className={`relative w-full pt-[56.25%] ${className}` }>
       <iframe
-        className="absolute top-0 left-0 w-full h-full shadow-lg"
+        className="absolute top-0 left-0 w-full h-full"
         src={`https://www.youtube.com/embed/${videoId}?autoplay=1&mute=1&loop=1&playlist=${videoId}&controls=0&modestbranding=1`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
